26a1a0d19500e6dc9ba9941d753a80283c038653
Srinivas <srinivas.teella@vivaenergy.com.au>
2023-06-23 21:12:56 +1000

    add remove

--- DepthChart/Service/NFLService.cs
+++ DepthChart/Service/NFLService.cs
@@ -16,16 +16,17 @@ namespace DepthChart.Service
 
     public class NFLService : INFLService
     {
+        NFL nflDepthChart;
         public NFLService()
         {
-
+            nflDepthChart = GetDepthChart();
         }
         public Task<Player> addPlayerToDepthChart(Position position)
         {
-            NFL nflDepthChart = GetDepthChart();
-            var result = nflDepthChart.Data.GetType().GetProperty(position.PositionType.ToString());
-            throw new NotImplementedException();
-
+            //var result = nflDepthChart.Data.GetType().GetProperty(position.PositionType.ToString());
+            int index = CheckIfPlayerAlreadyPresent(nflDepthChart.Data.LWR,position.Player.Name);
+            if (index == -1) AddPlayer(position);
+            else InsertPlayer(index,position);
         }
 
         public Task<Player> getBackups(Position position)
@@ -41,6 +42,58 @@ namespace DepthChart.Service
         public Task<Player> removePlayerFromDepthChart(Position position)
         {
             throw new NotImplementedException();
+        }
+
+        private bool AddPlayer(Position position)
+        {
+            try
+            {
+                nflDepthChart.Data.LWR.Add((LWR)position);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool InsertPlayer(int index, Position position)
+        {
+            try
+            {
+                nflDepthChart.Data.LWR.Insert(index,(LWR)position);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool RemovePlayer(int index)
+        {
+            try
+            {
+                nflDepthChart.Data.LWR.RemoveAt(index);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void SaveChanges()
+        {
+            string output = JsonConvert.SerializeObject(nflDepthChart, Formatting.Indented);
+            File.WriteAllText("NFLDepthChart.json", output);
+        }
+
+
+        private int CheckIfPlayerAlreadyPresent(List<LWR> data,string playerName)
+        {
+            int index = data.FindIndex(a => a.Player.Name == playerName);
+            return index;
         }
 
         private NFL GetDepthChart()
